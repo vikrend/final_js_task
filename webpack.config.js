3e851de724ddcd20a780b93db42728f444d90bcd
astrielov <krendelllllll@gmail.com>
2018-03-22 12:56:45 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/app.js',
+  devtool: 'inline-source-map',
+  watch: true,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
